8d1d8f2dd4c2c9e456801bf46b5b9c4b2ea510ac
Jahid1999 <bsse1030@iit.du.ac.bd>
2023-02-06 20:41:41 +0600

    lead profile card

--- resources/views/leads/view.blade.php
+++ resources/views/leads/view.blade.php
@@ -14,58 +14,95 @@
                          <img src="{{ asset('assets/img/profiles/profile.png') }}" alt="User" class="user-image" />
                      </div>
                      <div class="col">
-                         <span>Lead</span>
-                         <p class="font-weight-bold">{{$lead->name}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Lead</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->name}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Lead Phone</span>
-                         <p class="font-weight-bold">{{$lead->mobile}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Lead Phone</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->mobile}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Lead Email</span>
-                         <p class="font-weight-bold">{{$lead->email}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Lead Email</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->email}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Lead Status</span>
-                         <p class="font-weight-bold">{{$lead->status}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Lead Status</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->status}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Counsellor</span>
-                         <p class="font-weight-bold">{{$lead->owner?$lead->owner->name:'Unassigned'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Counsellor</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->owner?$lead->owner->name:'Unassigned'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                  </div>
                  <div class="col-md-6 m-auto">
                      <div class="col">
-                         <span>Intake Month / Year</span>
-                         <p class="font-weight-bold">{{$lead->intake_month_year ?? 'N/A'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Intake Month / Year</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->intake_month_year ?? 'N/A'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Last Education</span>
-                         <p class="font-weight-bold">
-                             {{$lead->last_education??'N/A'}}
-                             <i class="fa fa-info-circle" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="{{$lead->education_details??'N/A'}}"></i>
-                         </p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Last Education</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->last_education??'N/A'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Completion Date</span>
-                         <p class="font-weight-bold">{{$lead->completion_date??'N/A'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Completion Date</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->completion_date??'N/A'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>Job Experience</span>
-                         <p class="font-weight-bold">{{$lead->job_experience??'N/A'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Job Experience</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->job_experience??'N/A'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                      <div class="col">
-                         <span>English</span>
-                         <p class="font-weight-bold">{{$lead->english ?? 'N/A'}}</p>
-                     </div>
-                     <div class="col">
-                         <span>Englist Result</span>
-                         <p class="font-weight-bold">{{$lead->english_result??'N/A'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">English</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->english ?? 'N/A'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
 
                      <div class="col">
-                         <span>Creator</span>
-                         <p class="font-weight-bold">{{$lead->creator?$lead->creator->name:'NA'}}</p>
+                         <div class="form-group row">
+                             <div class="col-md-12"><label class="col-form-label">Creator</label></div>
+                             <div class="col-md-12">
+                                 <input id="edit-lead-name" class="form-control font-weight-bold" type="text" name="name" value="{{$lead->creator?$lead->creator->name:'NA'}}" required disabled>
+                             </div>
+                         </div>
                      </div>
                  </div>
 
