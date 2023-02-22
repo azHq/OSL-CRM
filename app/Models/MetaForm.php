3b600d243fde479fbfc5970269d7e17c27e789f8
RadowanMahmud <bsse1002@iit.du.ac.bd>
2023-02-23 01:33:56 +0600

    added model and migration for meta

---
+++ app/Models/MetaForm.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MetaForm extends Model
+{
+    use HasFactory;
+}
